cafcecbdbe15acdfa92eae0b35b7f48373050827
hehesky <lk.genesis@163.com>
2017-11-05 15:07:24 -0500

    sample usage of test

---
+++ main.py
@@ -0,0 +1,34 @@
+import bandit
+import agent
+
+class BanditTest(object):
+    def __init__(self,bandits,agents,n=1000):
+        """bandits: MultiArmedBandit object, agents: a list of agents, n: number of rounds to play"""
+        self.bandits=bandits
+        self.agents=agents
+        self.n=n
+    def run(self,n=None):
+        if n is not None:
+            self.n=n
+
+        for i in range(self.n):
+            pull_result=self.bandits.pull_all()
+            for agent in self.agents:
+                agent.play(pull_result)
+
+    def show_result(self):
+        raise NotImplementedError
+
+
+if __name__=="__main__":
+    bandit_list=[]
+    bandit_list.append(bandit.BernoulliBandit(0.5,1))
+    bandit_list.append(bandit.GaussianBandit(1,0.5))
+    bandit_num=len(bandit_list)
+    KArmedBandit=bandit.MultiArmedBandit(bandit_list)
+
+    agents=[agent.Agent(bandit_num)]
+
+    test=BanditTest(KArmedBandit,agents,1000)
+    test.run()
+
