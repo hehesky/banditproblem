7e56bede1e0f30435f17b1f86fd3ba57a8327627
Sherry Sun <33243725+sxy5256667@users.noreply.github.com>
2017-12-05 16:55:42 -0500

    testing

--- Data.py
+++ Data.py
@@ -42,8 +42,21 @@ def processLine(line):
         
     return display,click,user_vec
 
+def rewrite(data_dir):
+    wfile = open("rewrite.txt","w")
+    l = 0
+    with open(data_dir) as file:
+        for line in file:
+            wfile.write(line)
+            if(l<1000):
+                l+=1
+            else:
+                wfile.close()
+                return
+    return
 
 if __name__=='__main__':
+
     line = '1318722897 id-611110 0 |user 1 6 11 13 33 18 14 39 20 |id-596821 |id-600025 |id-604964 |id-605378 |id-605423 |id-605518 |id-605672 |id-606079 |id-606207 |id-606696 |id-606812 |id-606900 |id-610233 |id-610351 |id-610505 |id-610758 |id-611078 |id-611110 |id-611479 |id-611482 |id-611585 |id-611775 |id-611932 |id-612378 |id-612461 |id-612506 |id-613111 |id-613241 |id-613242 |id-613404 |id-613449 |id-613505 |id-613546 |id-613675 |id-613689 |id-613765 |id-613786'
     display,click,user_vec=processLine(line)
     print(display)
@@ -51,12 +64,6 @@ if __name__=='__main__':
     print(user_vec)
 
     data_dir = "data.txt"
+    #rewrite(data_dir)
     first_line,display,click,user_vec,pool = load_data(data_dir)
-    
-    print(first_line)
-    user_id = 1
-    print(display[user_id])
-    print(click[user_id])
-    print(user_vec[user_id])
-    print(pool)
     
--- Statistic.py
+++ Statistic.py
@@ -5,7 +5,7 @@ Created on Fri Dec  1 19:36:02 2017
 @author: sxy52
 """
 import numpy as np
-
+from numpy.random import choice
 class Statistic(object):
     def __init__(self,feature_num, arm_num):
         self.ft_num = feature_num
@@ -22,7 +22,7 @@ class Statistic(object):
         self.arm_count[arm] += click
         self.count += 1
         
-    def decide(self, ft_vec):
+    def predict(self, ft_vec):
         ft_vec = np.asarray(ft_vec)
         prob = np.zeros(self.arm_num)
         for i in range(self.arm_num):
@@ -31,9 +31,15 @@ class Statistic(object):
             n_prob = (self.record[i][condition == False] - self.arm_count[i])*(-1)
             n_cond_p = np.prod(n_prob/n_count)
             prob[i] = np.prod(self.record[i][condition]/self.ft_count[condition])*n_cond_p
-        
-        return prob
-        
-sta = Statistic(5,3)
+        decision=choice(np.flatnonzero(prob == prob.max()))
+        return decision
+
+if __name__=='__main__':
+    agent=Statistic(3,3)
+    context=np.array([1,2,3])
+    agent.learn(context,1,1)
+    agent.learn(context,0,0)
+    agent.learn(context,2,1)
+    print(agent.predict(context))
     
     
