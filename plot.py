ff5b921c6659ae0d8894ade62b64d847bb7d7a79
hehesky <lk.genesis@163.com>
2017-12-11 16:33:26 -0500

    update plot

--- plot.py
+++ plot.py
@@ -1,4 +1,5 @@
-
+import os
+import os.path
 try:
     import tkinter
 except ImportError:
@@ -14,7 +15,7 @@ def cumulative_avg(reward_record):
     cum_avg=cum_sum*1.0/indices
     return cum_avg
 
-def plot_avg(cum_avgs,title="Plot",filename=None,legend=None,xlabel=None,ylabel=None):
+def plot_avg(cum_avgs,title="Plot",filename=None,folder=None,legend=None,xlabel=None,ylabel=None):
     
     PLT.figure()
     for avg in cum_avgs:
@@ -28,10 +29,15 @@ def plot_avg(cum_avgs,title="Plot",filename=None,legend=None,xlabel=None,ylabel=
         PLT.ylabel(ylabel)
     if filename is None:
         filename=title+'.png'
-    PLT.savefig(filename)
+    if folder is not None:
+        if os.path.isdir(folder) is False: os.mkdir(folder)
+        path=os.path.join(folder,filename)
+    else:
+        path=filename
+    PLT.savefig(path)
 
 record=[0,1,0,1,0,0,1]
 avg=cumulative_avg(record)
 avg2=cumulative_avg([1,1,1,1,1,0,1])
 
-plot_avg([avg,avg2],"plot 1",legend=['line 1','line 2'],xlabel="x",ylabel='y')
+plot_avg([avg,avg2],"plot 1",folder='pic',legend=['line 1','line 2'],xlabel="x",ylabel='y')
